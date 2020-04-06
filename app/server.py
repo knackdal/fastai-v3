3d37e18b7f116423d0f29afecb66cf892e80162f
knackdal <63262991+knackdal@users.noreply.github.com>
2020-04-07 00:39:44 +0200

    Update server.py
    Commented original code and replaced with new code to replace the default bears model with the libcap model.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,15 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1OMs5cySCs7-1qGkCsKKn2CeD990xrcRs'
+export_file_name = 'libcap_trained_model'
 
-classes = ['black', 'grizzly', 'teddys']
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_name = 'export.pkl'
+
+classes = ['panaeolina_foenisecii', 'panaeolus_semiovatus', 'psilocybe_semilanceata']
+
+# classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
